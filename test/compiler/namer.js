3fa822a40fa8c7cd508ccd5c0c464a9b5d722491
Matt Basta <me@mattbasta.com>
2014-12-16 10:56:45 -0800

    Namer test

---
+++ test/compiler/namer.js
@@ -0,0 +1,19 @@
+var assert = require('assert');
+
+var namer = require('../../src/compiler/namer');
+
+
+describe('namer', function() {
+
+    it('should generate unique names always', function() {
+        var n = namer();
+        var data = {};
+        var name;
+        for (var i = 0; i < 10000; i++) {
+            name = n();
+            assert.ok(!(name in data));
+            data[name] = true;
+        }
+    });
+
+});
