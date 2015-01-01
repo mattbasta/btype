a4ce1fefcbb9f6eaffc8eca412c478375e4579a2
Matt Basta <me@mattbasta.com>
2014-12-31 16:39:10 -0800

    Continue support; more tests

--- test/parser.js
+++ test/parser.js
@@ -1745,6 +1745,106 @@ describe('Parser', function() {
                 ])
             );
         });
+
+        it('should be invalid outside of loops', function() {
+            assert.throws(function() {
+                parser(lexer('break;'));
+            });
+        });
+    });
+
+
+    describe('`continue` statements', function() {
+        it('should be valid within `for` loops', function() {
+            compareTree(
+                'for (x = 0; x < 10; x = x + 1;) {continue;}',
+                _root([
+                    node(
+                        'For',
+                        0,
+                        43,
+                        {
+                            assignment: node(
+                                'Assignment',
+                                5,
+                                11,
+                                {
+                                    base: _i('x'),
+                                    value: _int(0)
+                                }
+                            ),
+                            condition: node(
+                                'RelativeBinop',
+                                11,
+                                18,
+                                {
+                                    operator: '<',
+                                    left: _i('x'),
+                                    right: _int(10)
+                                }
+                            ),
+                            iteration: node(
+                                'Assignment',
+                                19,
+                                30,
+                                {
+                                    base: _i('x'),
+                                    value: node(
+                                        'Binop',
+                                        23,
+                                        29,
+                                        {
+                                            operator: '+',
+                                            left: _i('x'),
+                                            right: _int(1)
+                                        }
+                                    )
+                                }
+                            ),
+                            loop: [node('Continue', 33, 41, {})],
+                        }
+                    )
+                ])
+            );
+        });
+        it('should be valid within `while` loops', function() {
+            compareTree(
+                'while (x) {continue;}',
+                _root([
+                    node(
+                        'While',
+                        0,
+                        21,
+                        {
+                            condition: _i('x'),
+                            loop: [node('Continue', 11, 19, {})],
+                        }
+                    )
+                ])
+            );
+        });
+        it('should be valid within `do/while` loops', function() {
+            compareTree(
+                'do {continue;} while (x);',
+                _root([
+                    node(
+                        'DoWhile',
+                        0,
+                        25,
+                        {
+                            condition: _i('x'),
+                            loop: [node('Continue', 4, 12, {})],
+                        }
+                    )
+                ])
+            );
+        });
+
+        it('should be invalid outside of loops', function() {
+            assert.throws(function() {
+                parser(lexer('continue;'));
+            });
+        });
     });
 
     it('should fail on invalid assertions', function() {
