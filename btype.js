c876249e5b5a608907131c9777501b4246887c7a
Matt Basta <mattbasta@gmail.com>
2016-02-07 11:10:34 -0800

    WIP

--- btype.js
+++ btype.js
@@ -1,13 +1,22 @@
-require('babel-register');
+var fs = require('fs');
+var path = require('path');
 
-var compiler = require('./src/compiler/compiler');
+if (fs.existsSync(path.resolve(__dirname, 'build', 'btype.js'))) {
+    module.exports = require('./build/btype');
+
+} else {
+    require('babel-register');
+
+    var compiler = require('./src/compiler/compiler');
 
 
-module.exports = function(source, filename, target) {
-    target = target || 'js';
-    return compiler({
-        filename: filename,
-        format: target,
-        sourceCode: source,
-    });
-};
+    module.exports = function(source, filename, target) {
+        target = target || 'js';
+        return compiler({
+            filename: filename,
+            format: target,
+            sourceCode: source,
+        });
+    };
+
+}
